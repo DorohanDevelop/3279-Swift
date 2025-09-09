ee402ec0b9bb0e5915d9a86c5a2a4e60985ba5a0
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-09-09 11:06:36 +0300

    init

---
+++ MasterFishingBook/App/MasterFishingBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterFishingBookApp.swift
+//  MasterFishingBook
+//
+//  Created by Serhii Babchuk on 08.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MasterFishingBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MFLaunchView()
+        }
+    }
+}
---
+++ MasterFishingBook/Extensions/ShapeStyle+Extensions.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    public static var green1: Color { Color(hex: 0x259910) }
+    public static var green2: Color { Color(hex: 0x2AA814) }
+    public static var yellow1: Color { Color(hex: 0xFFD700) }
+    public static var yellow2: Color { Color(hex: 0xFFB800) }
+    public static var pink1: Color { Color(hex: 0xFF66CC) }
+    public static var pink2: Color { Color(hex: 0x8000FF) }
+    public static var white: Color { Color(hex: 0xFFFFFF) }
+    public static var blue1: Color { Color(hex: 0x011F88) }
+    public static var blue2: Color { Color(hex: 0x01208D) }
+    public static var blue3: Color { Color(hex: 0x001A78) }
+    public static var blue4: Color { Color(hex: 0x011F89) }
+}
