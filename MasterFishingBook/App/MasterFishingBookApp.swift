7ae77300d70214a4064c78c545731b8faba02d2e
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-09-16 12:02:02 +0300

    add grey part app

--- MasterFishingBook/App/MasterFishingBookApp.swift
+++ MasterFishingBook/App/MasterFishingBookApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct MasterFishingBookApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
-            MFLaunchView()
+            AppEntryPoint()
         }
     }
 }
--- MasterFishingBook/Extensions/ShapeStyle+Extensions.swift
+++ MasterFishingBook/Extensions/ShapeStyle+Extensions.swift
@@ -12,4 +12,7 @@ extension ShapeStyle where Self == Color {
     public static var blue2: Color { Color(hex: 0x01208D) }
     public static var blue3: Color { Color(hex: 0x001A78) }
     public static var blue4: Color { Color(hex: 0x011F89) }
+    public static var privacyGradientTop: Color { Color(hex: 0xE0EAFC) }
+    public static var privacyGradientBottom: Color { Color(hex: 0xCFDEF3) }
+    public static var strokeGreen: Color { Color(hex: 0x14E585) }
 }
